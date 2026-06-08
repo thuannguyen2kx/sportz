476a1f53aefcb951855a876c372803487d83a461
nguyenthanhthuan <thuan.nguyenthanh.contact@gmail.com>
2026-06-08 22:29:41 +0700

    setup project

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
